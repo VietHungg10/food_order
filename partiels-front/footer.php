a96121b82c0cb01eff61423d92e74d06c7875820
Viethungg10 <hungg12103@gmail.com>
2024-06-23 17:55:13 +0700

    food_order project

--- partiels-front/footer.php
+++ partiels-front/footer.php
@@ -2,10 +2,10 @@
         <div class="container text-center">
             <ul>
                 <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png" /></a>
+                    <a href="https://www.facebook.com/viethungg.10"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png" /></a>
                 </li>
                 <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a>
+                    <a href="https://www.instagram.com/viethungg.10/"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a>
                 </li>
                 <li>
                     <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" /></a>
@@ -15,7 +15,7 @@
     </section>
     <section class="footer">
         <div class="container text-center">
-            <p>All rights reserved. Designed By <a href="#">Vijay Thapa</a></p>
+            <p>All rights reserved. Designed By <a href="#">Nguyen Viet Hung</a></p>
         </div>
     </section>
     <!-- footer Section Ends Here -->
